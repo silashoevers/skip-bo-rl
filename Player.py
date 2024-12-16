b9e10dc7d2d14e3080a56ebff69fcb0c1cb6c98c
Wouter Visser <wouter.visser54@gmail.com>
2024-12-16 09:51:04 +0100

    Added mask code

--- Player.py
+++ Player.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import torch
+
 
 class Player(ABC):
     def __init__(self, game):
@@ -83,8 +85,91 @@ class HumanPlayer(Player):
 
 
 class ComputerPlayer(Player):
-    def __init__(self, game):
+    def __init__(self, game, device):
         super().__init__(game)
+        self.mask = torch.empty(
+            13 * 4  # For every card to every build pile, so first card 1 to build 0, then card 1 to build 1 etc.
+            + 13 * 4  # For every card to every discard pile, so first card 1 to discard 0, then card 1 to discard 1 etc.
+            + 4 * 4  # From every discard pile to every build pile, so discard 0 to build 0, then discard 0 to build 1 etc.
+            + 4  # From stock pile to every build pile, so first stock to build 0, then stock to build 1
+        ).to(device)
+        self.device = device
+        self.num_piles = 4
+
+    def compute_mask(self):
+        for card in range(1, 14):
+            for pile in range(4):
+                card_face = 'S' if card == 13 else card
+                self.mask[self.num_piles * card + pile] = self.check_hand_to_build(card_face, pile)
+        offset = 13 * 4
+        for card in range(1, 14):
+            card_face = 'S' if card == 13 else card
+            self.mask[offset + self.num_piles * card:
+                      offset + self.num_piles * card + self.num_piles] = self.check_hand_to_discard(
+                card_face, 0)  # Only need to check for 1, not for all piles, as it does not depend on the pile
+        offset = 13 * 4 + 13 * 4
+        for discard_pile in range(4):
+            for build_pile in range(4):
+                self.mask[offset + self.num_piles * discard_pile + build_pile] = self.check_discard_to_build(
+                    discard_pile, build_pile)
+        offset = 13 * 4 + 13 * 4 + 4 * 4
+        for build_pile in range(4):
+            self.mask[offset + build_pile] = self.check_stock_to_build(build_pile)
+
+    def can_play_from_stock(self, build_index):
+        offset = 13 * 4 + 13 * 4 + 4 * 4
+        self.mask[offset + build_index] = self.check_stock_to_build(build_index)
+
+    def next_card_in_hand(self, build_index):
+        next_card_value = self.game.get_top_of_build_pile(build_index) + 1
+        self.mask[self.num_piles * next_card_value + build_index] = self.check_hand_to_build(next_card_value,
+                                                                                             build_index)
+
+    def can_play_from_discard(self, build_index):
+        offset = 13 * 4 + 13 * 4
+        for discard_pile in range(4):
+            self.mask[offset + self.num_piles * discard_pile + build_index] = self.check_discard_to_build(
+                discard_pile, build_index)
+
+    # Update the mask for every type of action
+    def mask_update_hand_to_build(self, card_face, build_index):
+        # Check if we have the next card in hand
+        self.next_card_in_hand(build_index)
+        # Check if we can play any of our discard pile
+        self.can_play_from_discard(build_index)
+        # Check if we can play from the stock
+        self.can_play_from_stock(build_index)
+
+        # Check if we have the same card again for discard and build
+        card_face = 13 if card_face == 'S' else card_face
+        for build_pile in range(4):
+            self.mask[self.num_piles * card_face + build_pile] = self.check_hand_to_build(card_face, build_pile)
+        offset = 13 * 4
+        self.mask[offset + self.num_piles * card_face:
+                  offset + self.num_piles * card_face + self.num_piles] = self.check_hand_to_discard(
+            card_face, 0)
+
+    def mask_update_discard_to_build(self, discard_index, build_index):
+        # Check if we have the next card for the build pile in hand
+        self.next_card_in_hand(build_index)
+        # Check if we can play from stock
+        self.can_play_from_stock(build_index)
+        # Check if we can play from any discard pile to the build pile
+        self.can_play_from_discard(build_index)
+
+        # Check if we can play from our discard pile again
+        offset = 13 * 4 + 13 * 4
+        for build_pile in range(4):
+            self.mask[offset + self.num_piles * discard_index + build_pile] = self.check_discard_to_build(
+                discard_index, build_pile)
+
+    def mask_update_stock_to_build(self, build_index):
+        # Check if we have the next card in hand
+        self.next_card_in_hand(build_index)
+        # Check if we can play from discard pile to the build pile
+        self.can_play_from_discard(build_index)
+        # Check if we can play from the stock
+        self.can_play_from_stock(build_index)
 
     def play(self):
         pass
