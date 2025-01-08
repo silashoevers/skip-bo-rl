3687564b38ce328d40b7ab87c76fc2ce6c69ef8a
Silas <silashoevers@gmail.com>
2025-01-08 13:08:31 +0100

    Backported match case to support older Python version

--- Player.py
+++ Player.py
@@ -127,36 +127,36 @@ class HumanPlayer(Player):
         end_turn = False
         while not end_turn:
             self.print_game_state()
-            match input("What's your move? Type 'help' to get an overview of available commands.\n").split():
-                case ["discard", "build", discard_pile_index, build_pile_index]:
-                    discard_pile_index, build_pile_index = int(discard_pile_index), int(build_pile_index)
-                    if self.check_discard_to_build(discard_pile_index, build_pile_index):
-                        self.play_discard_to_build(discard_pile_index, build_pile_index)
-                    else:
-                       print("Move not legal")
-                case ["stock", "build", build_pile_index]:
-                    build_pile_index = int(build_pile_index)
-                    if self.check_stock_to_build(build_pile_index):
-                        self.play_stock_to_build(build_pile_index)
-                    else:
-                        print("Move not legal")
-                case ["hand", "build", card_face, build_pile_index]:
-                    # TODO Remove str to int transform (Face should be str, value should be int)
-                    card_face = int(card_face) if card_face.isdigit() else card_face
-                    if self.check_hand_to_build(card_face, int(build_pile_index)):
-                        self.play_hand_to_build(card_face, int(build_pile_index))
-                    else:
-                        print("Move not legal")
-                case ["hand", "discard", card_face, discard_pile_index]:
-                    card_face = int(card_face) if card_face.isdigit() else card_face
-                    if self.check_hand_to_discard(card_face, int(discard_pile_index)):
-                        self.play_hand_to_discard(card_face, int(discard_pile_index))
-                        end_turn = True
-                    else:
-                        print("Move not legal")
-                case _:  # Unknown command
-                    print("Invalid input")
-                    print(HELP_STRING)
+            input = input("What's your move? Type 'help' to get an overview of available commands.\n").split()
+            if input == ["discard", "build", discard_pile_index, build_pile_index]:
+                discard_pile_index, build_pile_index = int(discard_pile_index), int(build_pile_index)
+                if self.check_discard_to_build(discard_pile_index, build_pile_index):
+                    self.play_discard_to_build(discard_pile_index, build_pile_index)
+                else:
+                   print("Move not legal")
+            elif input == ["stock", "build", build_pile_index]:
+                build_pile_index = int(build_pile_index)
+                if self.check_stock_to_build(build_pile_index):
+                    self.play_stock_to_build(build_pile_index)
+                else:
+                    print("Move not legal")
+            elif input == ["hand", "build", card_face, build_pile_index]:
+                # TODO Remove str to int transform (Face should be str, value should be int)
+                card_face = int(card_face) if card_face.isdigit() else card_face
+                if self.check_hand_to_build(card_face, int(build_pile_index)):
+                    self.play_hand_to_build(card_face, int(build_pile_index))
+                else:
+                    print("Move not legal")
+            elif input == ["hand", "discard", card_face, discard_pile_index]:
+                card_face = int(card_face) if card_face.isdigit() else card_face
+                if self.check_hand_to_discard(card_face, int(discard_pile_index)):
+                    self.play_hand_to_discard(card_face, int(discard_pile_index))
+                    end_turn = True
+                else:
+                    print("Move not legal")
+            else:  # Unknown command
+                print("Invalid input")
+                print(HELP_STRING)
             # Check if the player has won, if so: end the game
             if len(self.stock_pile) < 1:
                 print("Your stock pile is empty, congratulations!")
