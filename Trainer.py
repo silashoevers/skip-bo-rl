d8f4ff08954a17aa4bddbac0a7187122b37a442f
KionaB <kionabijker@yahoo.nl>
2025-01-11 00:42:24 +0100

    Added the missing classes to train

--- Trainer.py
+++ Trainer.py
@@ -135,7 +135,7 @@ class Trainer:
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    classes = [StockComputerPlayer, WinOnlyComputerPlayer, WinStockComputerPlayer, DiscardComputerPlayer, DiscardWinComputerPlayer, DiscardWinComputerPlayer]
+    classes = [StockComputerPlayer, WinOnlyComputerPlayer, WinStockComputerPlayer, DiscardComputerPlayer, DiscardWinComputerPlayer, DiscardWinComputerPlayer,DiscardStockComputerPlayer,ComplexComputerPlayer]
     for class_ in classes:
         trainer = Trainer(class_, device)
         trainer.train()
